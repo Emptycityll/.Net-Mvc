4b470ca8ebda1b8a88389c3df6449267c286ee00
Emptycityll <2455944421@qq.com>
2022-04-20 11:28:07 +0800

    添加项目文件。

---
+++ MvcStudent/Migrations/20220420032554_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcStudent.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Temperature = table.Column<int>(type: "int", nullable: false),
+                    Tbrq = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "student");
+        }
+    }
+}
---
+++ MvcStudent/Models/student.cs
@@ -0,0 +1,19 @@
+﻿namespace MvcStudent.Models
+{
+    public class student
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Mobile { get; set; }
+        public int Temperature { get; set; }
+        public DateTime Tbrq { get; set; }
+
+
+
+
+    }
+}
